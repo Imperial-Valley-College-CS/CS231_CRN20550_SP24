673f62d92733d5fce9f8c820ea220f56e51ec10c
ortoct <104336410+ortoct@users.noreply.github.com>
2024-02-13 13:47:00 -0800

    Initial commit

---
+++ Ch12 Exception Handling and Text IO/Exceptions.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Exceptions
+{
+   public static void main(String[] args)
+   {
+      Scanner scan = new Scanner(System.in);
+      
+      
+      while(true)
+      {                  
+         try
+         {
+            System.out.println("How old are you?");
+            int age = scan.nextInt();
+            break;
+         }catch( InputMismatchException e )
+         {  
+            //eat carriage return and newline left over from pressing enter key
+            scan.next();     
+            System.out.println("Value entered was not an integer.");
+         }  
+      }    
+   }//end main   
+}//end class
