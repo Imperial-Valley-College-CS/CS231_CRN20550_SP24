b27fe1ae2f1fc7a92e087de2dc39d4d137051224
ortoct <104336410+ortoct@users.noreply.github.com>
2024-02-29 11:36:03 -0800

    Updated App (added MouseEvent)

--- Ch13_AbstractClassesAndInterfaces/App.java
+++ Ch13_AbstractClassesAndInterfaces/App.java
@@ -5,6 +5,8 @@ import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.event.*;
+import javafx.scene.input.*;
 
 public class App extends Application
 {
@@ -16,8 +18,13 @@ public class App extends Application
    @Override
    public void start(Stage stage){
       
+      //scene.setOnMouseClicked();
       root.getChildren().add( canvas );
       stage.setScene(scene);
       stage.show();
+   }//end start
+   
+   class MouseHandler implements EventHandler<MouseEvent>
+   {
    }
-}
+}//end App
