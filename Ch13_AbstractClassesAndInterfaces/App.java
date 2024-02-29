8cbddf981457343d3f38d55f9385f24c724fcf71
ortoct <104336410+ortoct@users.noreply.github.com>
2024-02-29 11:24:24 -0800

    Updated App

--- Ch13_AbstractClassesAndInterfaces/App.java
+++ Ch13_AbstractClassesAndInterfaces/App.java
@@ -10,9 +10,13 @@ public class App extends Application
 {
    Group root = new Group();
    Scene scene = new Scene(root);
+   Canvas canvas = new Canvas(600, 400);
+   GraphicsContext gc = canvas.getGraphicsContext2D();
+   
    @Override
-   public void start(Stage stage)
-   {
+   public void start(Stage stage){
+      
+      root.getChildren().add( canvas );
       stage.setScene(scene);
       stage.show();
    }
